f5fe44f5235b363298552683682a23679798a20f
Oscar BOOG <oscar-loic-peresse.boog@ens.uvsq.fr>
2025-03-07 14:47:02 +0100

    Commit some changes

---
+++ build/CMakeFiles/path_tracer.dir/DependInfo.cmake
@@ -0,0 +1,24 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/sletter/Musique/important/path_tracer/src/camera/camera.cpp" "CMakeFiles/path_tracer.dir/src/camera/camera.cpp.o" "gcc" "CMakeFiles/path_tracer.dir/src/camera/camera.cpp.o.d"
+  "/home/sletter/Musique/important/path_tracer/src/main.cpp" "CMakeFiles/path_tracer.dir/src/main.cpp.o" "gcc" "CMakeFiles/path_tracer.dir/src/main.cpp.o.d"
+  "/home/sletter/Musique/important/path_tracer/src/math/matrix.cpp" "CMakeFiles/path_tracer.dir/src/math/matrix.cpp.o" "gcc" "CMakeFiles/path_tracer.dir/src/math/matrix.cpp.o.d"
+  "/home/sletter/Musique/important/path_tracer/src/math/ray.cpp" "CMakeFiles/path_tracer.dir/src/math/ray.cpp.o" "gcc" "CMakeFiles/path_tracer.dir/src/math/ray.cpp.o.d"
+  "/home/sletter/Musique/important/path_tracer/src/math/vector.cpp" "CMakeFiles/path_tracer.dir/src/math/vector.cpp.o" "gcc" "CMakeFiles/path_tracer.dir/src/math/vector.cpp.o.d"
+  "/home/sletter/Musique/important/path_tracer/src/utils/random.cpp" "CMakeFiles/path_tracer.dir/src/utils/random.cpp.o" "gcc" "CMakeFiles/path_tracer.dir/src/utils/random.cpp.o.d"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ build/CMakeFiles/path_tracer.dir/cmake_clean.cmake
@@ -0,0 +1,21 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/path_tracer.dir/src/camera/camera.cpp.o"
+  "CMakeFiles/path_tracer.dir/src/camera/camera.cpp.o.d"
+  "CMakeFiles/path_tracer.dir/src/main.cpp.o"
+  "CMakeFiles/path_tracer.dir/src/main.cpp.o.d"
+  "CMakeFiles/path_tracer.dir/src/math/matrix.cpp.o"
+  "CMakeFiles/path_tracer.dir/src/math/matrix.cpp.o.d"
+  "CMakeFiles/path_tracer.dir/src/math/ray.cpp.o"
+  "CMakeFiles/path_tracer.dir/src/math/ray.cpp.o.d"
+  "CMakeFiles/path_tracer.dir/src/math/vector.cpp.o"
+  "CMakeFiles/path_tracer.dir/src/math/vector.cpp.o.d"
+  "CMakeFiles/path_tracer.dir/src/utils/random.cpp.o"
+  "CMakeFiles/path_tracer.dir/src/utils/random.cpp.o.d"
+  "path_tracer"
+  "path_tracer.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/path_tracer.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
